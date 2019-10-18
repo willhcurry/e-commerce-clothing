cdc0f534f970a83c7217e6bc502b1900128669e6
willhcurry <willhcurry@gmail.com>
2019-10-18 03:22:14 -0600

    updated preview

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionItem from '../CollectionItem/CollectionItem';
+
 import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
@@ -9,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
             {
                 items
                     .filter((item, index) => index < 4)
-                    .map((item) => (
-                        <div key={item.id}>{item.name}</div>
+                    .map(({ id, ...otherItemProps }) => (
+                        <CollectionItem key={ id } { ...otherItemProps } />
                 ))
             }
         </div>
